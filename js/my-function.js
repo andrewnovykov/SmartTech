b6cece6226ee239f593468899b60ada4ea8dc503
papapin <papapin777#gmail.com>
2018-08-01 12:21:53 -0700

    day6-1

--- js/my-function.js
+++ js/my-function.js
@@ -45,3 +45,36 @@ function workLoad(){
 	});
 
 }
+
+
+$('#smooth-scroll-menu  > a[href*="#"]:not([href="#"])').click(function () {
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+			return false;
+		}
+	}
+});
+
+$('#smooth-scroll-menu-2 > div > a[href*="#"]:not([href="#"])').click(function() {
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+      return false;
+    }
+  }
+});
